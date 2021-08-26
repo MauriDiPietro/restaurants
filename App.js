eca477f4cdfe88f207a3c57f30ea8fdd3baa51af
MauriDiPietro <dipietro.jm@gmail.com>
2021-08-26 17:34:44 -0300

    image-button

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {Text, View, StyleSheet, Image, Alert, TouchableOpacity} from 'react-native';
 
-export default function App() {
+
+const App = () =>{
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <Text style={styles.title}>Hola!</Text>
+        <Image source={{uri: 'https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2019/02/04125951/Playas-Brasil-9.jpg'}}
+        style={styles.image}/>
+        <TouchableOpacity 
+        style={styles.button}
+        onPress={()=>Alert.alert('Hola!')} >
+          <Text style={styles.textButton} >Presiona</Text>
+        </TouchableOpacity>
+        
     </View>
-  );
+  ) 
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  button: {backgroundColor: 'blue', padding: 7, marginTop: 10},
+  textButton: {color:'white', fontSize: 20},
+  container: {flex: 1, justifyContent: 'center', alignItems:'center', backgroundColor: 'cyan'},
+  title: {fontSize: 50, color: 'white'},
+  image: {height: 200, width: 200, borderRadius: 100}
+})
+
+export default App;
